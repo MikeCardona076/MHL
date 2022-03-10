6be45ba6171a6dcac65fae859d4697de08800d8c
Mike Cardona <40007337+MikeCardona076@users.noreply.github.com>
2022-03-10 07:19:17 -0800

    Ultimos Cambios

--- mhl/settings.py
+++ mhl/settings.py
@@ -102,7 +102,7 @@ def get_db():
     try:
         return {
             'default': {
-                'ENGINE': 'django.db.backends.sqlit3',
+                'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': BASE_DIR / 'db.sqlite3',
             }
             
@@ -112,7 +112,7 @@ def get_db():
         
         return {
             'default': {
-                'ENGINE': 'django.db.backends.sqlit3',
+                'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': BASE_DIR / 'db.sqlite3',
             }
             
